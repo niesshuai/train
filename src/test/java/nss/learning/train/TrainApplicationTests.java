13b32a7cf6d18c12225d8365d4995e660aae94d1
niesshuai <niesshuai1991@outlook.com>
2020-05-18 10:33:52 +0800

    init

---
+++ src/test/java/nss/learning/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package nss.learning.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
